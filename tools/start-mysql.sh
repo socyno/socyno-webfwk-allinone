8b5fd58a91996efd8bfde558656df11de44a6945
Socyno Tsai <socyno@outlook.com>
2020-12-31 22:28:22 +0800

    优化mysql配置参数

--- tools/start-mysql.sh
+++ tools/start-mysql.sh
@@ -5,4 +5,9 @@ docker run -p 3306:3306 --name mysql \
     -v /opt/mysql/logs:/var/log/mysql \
     -v /opt/mysql/data:/var/lib/mysql \
     -e MYSQL_ROOT_PASSWORD=adminp@ssword \
-    -d mysql:5.7
+    -d mysql:5.7 \
+    --character-set-server=utf8mb4 \
+    --collation-server=utf8mb4_general_ci \
+    --sql-mode STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION
+    
+
